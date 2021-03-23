85a20e20baad4c09800496b42e17a6fe9f9cb7c8
TJ <tjchase34@gmail.com>
2021-03-23 10:01:32 -0400

    Added glob crate, first arg is img directory path, dataflow args now start at 2

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
+use std::env;
+use glob::glob;
+
 mod base;
 mod orb;
 
 fn main() {
-    let img_paths = ["data/1.png".to_string(), "data/2.png".to_string()];
+    let args: Vec<String> = env::args().collect();
+    let img_dir = args[1].to_owned();
+    let mut glob_str = img_dir.to_owned();
+    glob_str.push_str("/*.png");
 
-    orb::orb_extract(img_paths.to_vec());
+    let mut img_paths = Vec::new();
+
+    for entry in glob(&glob_str).expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path) => match path.to_str() {
+                Some(path_str) => img_paths.push(path_str.to_owned()),
+                None => println!("Invalid path found!"),
+            },
+            Err(e) => println!("{:?}", e),
+        }
+    }
+
+    orb::orb_extract(img_paths);
 }
--- src/orb.rs
+++ src/orb.rs
@@ -1,9 +1,8 @@
 extern crate timely;
 use timely::dataflow::{InputHandle, ProbeHandle};
-use timely::dataflow::operators::{Inspect, Probe};
-
 // use timely::dataflow::channels::pact::Exchange;
-use timely::dataflow::operators::Exchange;
+use timely::dataflow::Scope;
+use timely::dataflow::operators::*;
 
 use std::hash::{Hash, Hasher};
 use std::collections::hash_map::DefaultHasher;
@@ -32,7 +31,7 @@ use opencv::{
 pub fn orb_extract(img_paths: Vec<String>) {
 
     // initializes and runs a timely dataflow.
-    timely::execute_from_args(std::env::args(), move |worker| {
+    timely::execute_from_args(std::env::args().skip(2), move |worker| {
 
         let index = worker.index();
 
@@ -45,8 +44,6 @@ pub fn orb_extract(img_paths: Vec<String>) {
             input.to_stream(scope)
                 .exchange(|x: &String| my_hash(x))
                 .inspect(move |x| {
-                    // println!("worker: {}, seen: {:?}", index, x);
-
                     // Read image
                     let img = imgcodecs::imread(x, imgcodecs::IMREAD_COLOR).unwrap();
                     let mut orb: PtrOfORB = ORB::default().unwrap();
@@ -55,20 +52,9 @@ pub fn orb_extract(img_paths: Vec<String>) {
 
                     orb.detect_and_compute(&img,&Mat::default().unwrap(), &mut kp, &mut des, false).unwrap();
                     println!("worker {}, processed {}, found {} keypoints",index, x, kp.len());
-
-                    // let mut out = core::Mat::default().unwrap();
-                    // features2d::draw_keypoints(&img, &kp, &mut out, core::Scalar::all(-1.0), features2d::DrawMatchesFlags_DRAW_RICH_KEYPOINTS).unwrap();
-
-                    // // Display image
-                    // highgui::named_window("test", highgui::WINDOW_AUTOSIZE).unwrap();
-                    // highgui::imshow("test", &out).unwrap();
-                    // highgui::wait_key(0).unwrap();
                 })
                 .probe_with(&mut probe);
         });
-
-        // feed the dataflow with data.
-        // let img_paths = ["data/1.png".to_string(), "data/2.png".to_string()];
 
         for i in 0..img_paths.len() {
             if index == 0 {
