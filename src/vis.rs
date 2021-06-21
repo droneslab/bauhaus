5b37592923a5b298dfcb2d47c2d7a8578885748e
Josh Jeong <djjeong127@gmail.com>
2021-06-21 10:20:23 -0700

    created the skeleton file for visualization actor

--- src/vis.rs
+++ src/vis.rs
@@ -17,34 +17,27 @@ use serde::{Deserialize, Serialize};
 
 // Message type for the actor
 #[derive(Debug, Serialize, Deserialize)]
-pub struct OrbMsg {
+pub struct VisMsg {
     // The ID of the actor that sent this message
     aid: Aid,
     // Vector of image paths to read in/extract
-    img_paths: Vec<String>,
+    img_pose: Pose,
 }
 
-impl OrbMsg {
+impl VisMsg {
     pub fn new(aid: Aid, vec: Vec<String>) -> Self {
         Self {
             aid,
-            img_paths: vec,
+            img_pose: pose,
         }
     }
 }
 
 // This is the handler that will be used by the actor.
-pub async fn orb_extract(_: (), context: Context, message: Message) -> ActorResult<()> {
-    if let Some(msg) = message.content_as::<OrbMsg>() {
+pub async fn Vis_extract(_: (), context: Context, message: Message) -> ActorResult<()> {
+    if let Some(msg) = message.content_as::<VisMsg>() {
         println!("{:?}", context);
-        for path in &msg.img_paths {
-            let img = imgcodecs::imread(path, imgcodecs::IMREAD_COLOR).unwrap();
-            let mut orb: PtrOfORB = ORB::default().unwrap();
-            let mut kp = VectorOfKeyPoint::new();
-            let mut des = Mat::default();
-
-            orb.detect_and_compute(&img,&Mat::default(), &mut kp, &mut des, false).unwrap();
-            println!("Processed {}, found {} keypoints", path, kp.len());
+        
         }
         context.system.trigger_shutdown();
     }
