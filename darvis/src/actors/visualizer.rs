2dc5860a1c214aa2b3b7fd2ed6cd5dab053d764a
Sofiya Semenova <novasofiya@gmail.com>
2023-08-22 20:05:53 -0400

    Got visualizer workflow going

--- darvis/src/actors/visualizer.rs
+++ darvis/src/actors/visualizer.rs
@@ -1,9 +1,11 @@
 use axiom::{message::ActorMessage, prelude::*};
 use dvcore::{plugin_functions::Function,matrix::*,};
-use r2r::builtin_interfaces::msg::Duration;
-use r2r::std_msgs::msg::Int32;
-use r2r::trajectory_msgs::msg::*;
-use r2r::QosProfile;
+use rerun::{
+    components::{ColorRGBA, Point3D, Radius},
+    demo_util::grid,
+    external::glam,
+    MsgSender, RecordingStreamBuilder, RecordingStream,
+};
 
 use crate::{
     actors::messages::VisMsg, 
@@ -11,72 +13,52 @@ use crate::{
     lockwrap::ReadOnlyWrapper
 };
 
-use super::messages::VisPathMsg;
 
 #[derive(Debug, Clone)]
 pub struct DarvisVisualizer {
     map: ReadOnlyWrapper<Map>,
-    // node: r2r::Node,
-    // publisher: r2r::Publisher<r2r::std_msgs::msg::String>,
-    // ctx: r2r::Context
+    rec_stream: RecordingStream
 }
 
 impl DarvisVisualizer {
-    pub fn new(map: ReadOnlyWrapper<Map>) -> DarvisVisualizer {
-        return DarvisVisualizer{map};
+    pub fn new(map: ReadOnlyWrapper<Map>, rec_stream: RecordingStream) -> DarvisVisualizer {
+        let points = grid(glam::Vec3::splat(-10.0), glam::Vec3::splat(10.0), 10)
+            .map(Point3D::from)
+            .collect::<Vec<_>>();
+        let colors = grid(glam::Vec3::ZERO, glam::Vec3::splat(255.0), 10)
+            .map(|v| ColorRGBA::from_rgb(v.x as u8, v.y as u8, v.z as u8))
+            .collect::<Vec<_>>();
 
-        // let ctx = r2r::Context::create().unwrap();
-        // let mut node = r2r::Node::create(r2r::Context::create().unwrap(), "testnode", "").unwrap();
-        // // let publisher = node.create_publisher::<JointTrajectoryPoint>("/jtp", QosProfile::default()).unwrap();
-        // let publisher2 = node.create_publisher::<r2r::std_msgs::msg::String>("/test", QosProfile::default()).unwrap();
+        MsgSender::new("my_points")
+            .with_component(&points).unwrap()
+            .with_component(&colors).unwrap()
+            .with_splat(Radius(0.5)).unwrap()
+            .send(&rec_stream).unwrap();
 
 
-        // let visualizer = DarvisVisualizer {
-        //     map,
-        //     node,
-        //     publisher: publisher2,
-        //     ctx
-        // };
-
-        // let msg = r2r::std_msgs::msg::String { data: format!("Hello, world!") };
-        // publisher2.publish(&msg).unwrap();
-
-        // // // run for 10 seconds
-        // // let mut count = 0;
-        // // let mut positions: Vec<f64> = Vec::new();
-        // // while count < 100 {
-        // //     positions.push(count as f64);
-        // //     let to_send = JointTrajectoryPoint {
-        // //         positions: positions.clone(),
-        // //         time_from_start: Duration {
-        // //             sec: count,
-        // //             nanosec: 0,
-        // //         },
-        // //         ..Default::default()
-        // //     };
-        // //     let mut native = r2r::NativeMsg::<Int32>::new();
-        // //     native.data = count;
-
-        // //     publisher.publish(&to_send).unwrap();
-
-        // //     std::thread::sleep(std::time::Duration::from_millis(100));
-        // //     count += 1;
-        // // }
-
-        // visualizer
+        return DarvisVisualizer{map, rec_stream};
     }
 
     pub fn visualize(&mut self, _context: Context, message: Message) -> ActorResult<()> {
+        let points = grid(glam::Vec3::splat(-10.0), glam::Vec3::splat(10.0), 10)
+            .map(Point3D::from)
+            .collect::<Vec<_>>();
+        let colors = grid(glam::Vec3::ZERO, glam::Vec3::splat(255.0), 10)
+            .map(|v| ColorRGBA::from_rgb(v.x as u8, v.y as u8, v.z as u8))
+            .collect::<Vec<_>>();
 
-        // if let Some(msg) = message.content_as::<VisMsg>()  {}
+        MsgSender::new("my_points")
+            .with_component(&points).unwrap()
+            .with_component(&colors).unwrap()
+            .with_splat(Radius(0.5)).unwrap()
+            .send(&self.rec_stream).unwrap();
 
         Ok(Status::done(()))
     }
 }
 
 impl Function for DarvisVisualizer {
-    fn handle(&mut self, _context: axiom::prelude::Context, message: Message) -> ActorResult<()>
-    {
+    fn handle(&mut self, _context: axiom::prelude::Context, message: Message) -> ActorResult<()> {
         self.visualize(_context, message).unwrap();
         Ok(Status::done(()))
     }
